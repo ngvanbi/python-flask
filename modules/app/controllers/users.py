f62d680ac299c42c7095d2b516dbcf0dae33fcb4
Nguyen Van Bi <ngvanbi91@gmail.com>
2019-09-17 00:56:00 +0700

    Update users.py

--- modules/app/controllers/users.py
+++ modules/app/controllers/users.py
@@ -1,15 +1,37 @@
 import os
 
 from flask import jsonify, request
-
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required,
+                                get_jwt_identity)
 import logger
-from app import app, mongo
+from app import app, mongo, flask_bcrypt, jwt
 from app.schemas import validate_user
 
 ROOT_PATH = os.environ.get('ROOT_PATH')
 LOG = logger.get_root_logger(
     __name__, filename=os.path.join(ROOT_PATH, 'output.log')
 )
+
+
+@app.route("/auth", methods=['POST'])
+def auth_user():
+    """Auth endpoint."""
+    data = validate_user(request.get_json())
+    if data.get('ok'):
+        data = data.get('data')
+        user_data = mongo.db.users.find_one({'email': data.get('email')}, {'_id': 0})
+        LOG.debug(user_data)
+        if user_data and flask_bcrypt.check_password_hash(user_data.get('password'), data.get('password')):
+            del user_data['password']
+            access_token = create_access_token(identity=data)
+            refresh_token = create_refresh_token(identity=data)
+            user_data['token'] = access_token
+            user_data['refresh'] = refresh_token
+            return jsonify({'ok': True, 'data': user_data}), 200
+        else:
+            return jsonify({'ok': False, 'message': 'Invalid username or password'}), 401
+    else:
+        return jsonify({'ok': False, 'message': 'Bad request parameters: {}'.format(data.get('message'))}), 400
 
 
 @app.route('/user', methods=['GET', 'POST', 'DELETE', 'PATCH'])
